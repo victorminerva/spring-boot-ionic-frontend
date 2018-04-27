123d61c2833564df01147bc593f399eaf9910eea
Victor Minerva <victorminerva.m@gmail.com>
2018-04-26 21:45:42 -0300

    Interceptor para incluir token nas requisições

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
 import { ClienteService } from '../services/domain/cliente.service';
+import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { ClienteService } from '../services/domain/cliente.service';
     AuthService,
     StorageService,
     ClienteService,
+    AuthInterceptorProvider,
     ErrorInterceptorProvider
   ]
 })
