990296bdfcdde0a8da0ab94422ca108b1ac1d6de
Victor Minerva <victorminerva.m@gmail.com>
2018-05-01 14:33:44 -0300

    Criando página de carrinho de compras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { StorageService } from '../services/storage.service';
 import { ClienteService } from '../services/domain/cliente.service';
 import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
 import { ProdutoService } from '../services/domain/produto.service';
+import { CartService } from '../services/domain/cart.service';
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { ProdutoService } from '../services/domain/produto.service';
     StorageService,
     ClienteService,
     ProdutoService,
+    CartService,
     AuthInterceptorProvider,
     ErrorInterceptorProvider
   ]
--- src/services/storage.service.ts
+++ src/services/storage.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { LocalUser } from "../models/localuser";
 import { STORAGE_KEYS } from "../config/storage_keys.config";
+import { Cart } from "../models/cart";
 
 Injectable()
 export class StorageService {
@@ -12,7 +13,6 @@ export class StorageService {
         } else {
             return JSON.parse(user);
         }
-
     }
 
     setLocalUser(user: LocalUser) {
@@ -20,6 +20,23 @@ export class StorageService {
             localStorage.removeItem(STORAGE_KEYS.localUser);
         } else {
             localStorage.setItem(STORAGE_KEYS.localUser, JSON.stringify(user));
+        }
+    }
+
+    getCart() : Cart {
+        let cart = localStorage.getItem(STORAGE_KEYS.cart);
+        if (cart == null) {
+            return null;
+        } else {
+            return JSON.parse(cart);
+        }
+    }
+
+    setCart(cart: Cart) {
+        if(cart == null) {
+            localStorage.removeItem(STORAGE_KEYS.cart);
+        } else {
+            localStorage.setItem(STORAGE_KEYS.cart, JSON.stringify(cart));
         }
     }
 }
